02fc73ecc2396d7fcf5f7ccb15354640f02f5846
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 20:17:02 +0500

    removed unnecessary imports

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 chrome_options = webdriver.ChromeOptions()
